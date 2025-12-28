573b83fce701ccf60feefb49e12974444e059fc2
Cyprian1830 <83276124+Cyprian1830@users.noreply.github.com>
2025-12-28 19:56:21 +0100

    Add files via upload

--- main.tf
+++ main.tf
@@ -15,21 +15,9 @@ provider "azurerm" {
   features {}
 }
 
-
 resource "azurerm_resource_group" "rg" {
   name     = "rg-project"
-  # ZMIANA: Zaktualizowano region na dostępny w subskrypcji studenckiej
   location = "Poland Central"
-}
-
-resource "azurerm_storage_account" "storage" {
-  name                     = "iadlabstorage${random_string.random.result}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  is_hns_enabled = true
 }
 
 resource "random_string" "random" {
@@ -37,4 +25,3 @@ resource "random_string" "random" {
   special = false
   upper   = false
 }
-
