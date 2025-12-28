0db8f88e773a5775fdf0ceec5a79c8405ac569ea
Cyprian1830 <83276124+Cyprian1830@users.noreply.github.com>
2025-12-28 11:53:02 +0100

    Add files via upload

---
+++ postgresql.tf
@@ -0,0 +1,71 @@
+# =========================
+# RANDOM SUFFIX (NAZWY)
+# =========================
+resource "random_string" "pg_suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
+# =========================
+# POSTGRESQL FLEXIBLE SERVER
+# =========================
+resource "azurerm_postgresql_flexible_server" "pg" {
+  name                = "pg-sales-${random_string.pg_suffix.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  administrator_login    = "pgadmin"
+  administrator_password = var.postgres_admin_password
+
+  version = "14"
+
+  sku_name   = "B_Standard_B1ms"   # tania opcja (dev/lab)
+  storage_mb = 32768               # 32 GB
+
+  # Publiczny dostęp tylko na potrzeby projektu/labu
+  public_network_access_enabled = true
+
+  tags = {
+    project = "real-time-sales"
+    env     = "dev"
+  }
+}
+
+# =========================
+# DATABASE
+# =========================
+resource "azurerm_postgresql_flexible_server_database" "db" {
+  name      = "salesanalytics"
+  server_id = azurerm_postgresql_flexible_server.pg.id
+  charset   = "UTF8"
+}
+
+# =========================
+# FIREWALL – DEV / LAB ONLY
+# =========================
+resource "azurerm_postgresql_flexible_server_firewall_rule" "allow_all" {
+  name             = "allow-all-dev"
+  server_id        = azurerm_postgresql_flexible_server.pg.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "255.255.255.255"
+}
+
+# =========================
+# OUTPUTS
+# =========================
+output "postgres_server_name" {
+  value = azurerm_postgresql_flexible_server.pg.name
+}
+
+output "postgres_fqdn" {
+  value = azurerm_postgresql_flexible_server.pg.fqdn
+}
+
+output "postgres_database" {
+  value = azurerm_postgresql_flexible_server_database.db.name
+}
+
+output "postgres_user" {
+  value = azurerm_postgresql_flexible_server.pg.administrator_login
+}
